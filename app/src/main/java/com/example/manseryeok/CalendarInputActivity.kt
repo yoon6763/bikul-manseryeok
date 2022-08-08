af169a341e58f15bb60ab129b64220f0143c8fdf
Yoon <yoon6763@naver.com>
2022-08-08 15:28:48 +0900

    MainPage Layout Init and CalendarInputActivity Added.

---
+++ app/src/main/java/com/example/manseryeok/CalendarInputActivity.kt
@@ -0,0 +1,17 @@
+package com.example.manseryeok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.manseryeok.databinding.ActivityCalendarInputBinding
+
+class CalendarInputActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityCalendarInputBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+
+    }
+}
--- app/src/main/java/com/example/manseryeok/MainActivity.kt
+++ app/src/main/java/com/example/manseryeok/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.manseryeok
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.manseryeok.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
+        binding.run {
+            clCalendar.setOnClickListener(this@MainActivity)
+            clCompass.setOnClickListener(this@MainActivity)
+            clDatabase.setOnClickListener(this@MainActivity)
+            clName.setOnClickListener(this@MainActivity)
+            clMedia.setOnClickListener(this@MainActivity)
+            clQuestion.setOnClickListener(this@MainActivity)
+        }
+    }
+
+
+    override fun onClick(p0: View?) {
+        binding.run {
+            when (p0?.id) {
+                clCalendar.id -> {
+
+                }
+            }
+        }
     }
 }
