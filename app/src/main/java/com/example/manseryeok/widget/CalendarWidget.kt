719f6952898f92202c2b2a3ec4c757afe3f75fdc
uknow-mac <yoon6763@naver.com>
2023-10-25 17:48:18 +0900

    fix:위젯 클릭 시 앱 실행하도록 변경

--- app/src/main/java/com/example/manseryeok/widget/CalendarWidget.kt
+++ app/src/main/java/com/example/manseryeok/widget/CalendarWidget.kt
@@ -1,13 +1,18 @@
 package com.example.manseryeok.widget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
 import com.example.manseryeok.R
 import com.example.manseryeok.manseryeokdb.ManseryeokSQLHelper
+import com.example.manseryeok.page.MainActivity
 import com.example.manseryeok.utils.Utils
+import com.gun0912.tedpermission.provider.TedPermissionProvider
 import java.util.Calendar
+
 
 class CalendarWidget : AppWidgetProvider() {
 
@@ -36,7 +41,6 @@ internal fun updateAppWidget(
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-
     val manseryeokSQLHelper = ManseryeokSQLHelper(context)
     manseryeokSQLHelper.open()
 
@@ -59,11 +63,15 @@ internal fun updateAppWidget(
 
     sb.append("${timeGanji[0]}${dayGanji[0]}${monthGanji[0]}${yearGanji[0]}\n${timeGanji[1]}${dayGanji[1]}${monthGanji[1]}${yearGanji[1]}")
 
-
     val widgetText = sb.toString()
     val views = RemoteViews(context.packageName, R.layout.calendar_widget)
     views.setTextViewText(R.id.tv_widget_calendar, widgetText)
     views.setTextViewText(R.id.tv_widget_calendar_date, "${year}년 ${month}월 ${day}일")
+
+    val intent = Intent(TedPermissionProvider.context, MainActivity::class.java) //실행할 액티비티의 클래스
+
+    val pendingIntent = PendingIntent.getActivity(TedPermissionProvider.context, 0, intent, 0)
+    views.setOnClickPendingIntent(R.id.widget_calendar_container, pendingIntent)
 
     appWidgetManager.updateAppWidget(appWidgetId, views)
 }
