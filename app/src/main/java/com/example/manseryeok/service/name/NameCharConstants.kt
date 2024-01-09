98dbe66f0133c67e76bbe5377fc59ec461861578
uknow-home <yoon6763@naver.com>
2024-01-09 22:07:07 +0900

    fix: 획수 오기입 변경

--- app/src/main/java/com/example/manseryeok/service/name/NameCharConstants.kt
+++ app/src/main/java/com/example/manseryeok/service/name/NameCharConstants.kt
@@ -35,16 +35,16 @@ object NameCharConstants {
 
     // 획수
     val scoreHashMap = hashMapOf(
-        ' ' to 0, ' ' to 0, 'ㄱ' to 2, 'ㄲ' to 4, 'ㄴ' to 2,
-        'ㄷ' to 3, 'ㄸ' to 6, 'ㄹ' to 5, 'ㅁ' to 4, 'ㅂ' to 4,
+        ' ' to 0, ' ' to 0, 'ㄱ' to 1, 'ㄲ' to 2, 'ㄴ' to 1,
+        'ㄷ' to 2, 'ㄸ' to 4, 'ㄹ' to 3, 'ㅁ' to 3, 'ㅂ' to 4,
         'ㅃ' to 8, 'ㅅ' to 2, 'ㅆ' to 4, 'ㅇ' to 1, 'ㅈ' to 3,
-        'ㅉ' to 6, 'ㅊ' to 4, 'ㅋ' to 3, 'ㅌ' to 4, 'ㅍ' to 4,
-        'ㅎ' to 3, 'ㄳ' to 4, 'ㄵ' to 5, 'ㄶ' to 5, 'ㄺ' to 7,
-        'ㄻ' to 9, 'ㄼ' to 9, 'ㄽ' to 7, 'ㄾ' to 9, 'ㄿ' to 9,
-        'ㅀ' to 8, 'ㅄ' to 6, 'ㅏ' to 2, 'ㅐ' to 3, 'ㅑ' to 3,
+        'ㅉ' to 6, 'ㅊ' to 4, 'ㅋ' to 2, 'ㅌ' to 3, 'ㅍ' to 4,
+        'ㅎ' to 3, 'ㄳ' to 3, 'ㄵ' to 4, 'ㄶ' to 4, 'ㄺ' to 4,
+        'ㄻ' to 7, 'ㄼ' to 7, 'ㄽ' to 5, 'ㄾ' to 6, 'ㄿ' to 7,
+        'ㅀ' to 6, 'ㅄ' to 6, 'ㅏ' to 2, 'ㅐ' to 3, 'ㅑ' to 3,
         'ㅒ' to 4, 'ㅓ' to 2, 'ㅔ' to 3, 'ㅕ' to 3, 'ㅖ' to 4,
         'ㅗ' to 2, 'ㅘ' to 4, 'ㅙ' to 5, 'ㅚ' to 3, 'ㅛ' to 3,
         'ㅜ' to 2, 'ㅝ' to 4, 'ㅞ' to 5, 'ㅟ' to 3, 'ㅠ' to 3,
-        'ㅡ' to 1, 'ㅢ' to 2, 'ㅣ' to 1,
+        'ㅡ' to 1,    'ㅢ' to 2, 'ㅣ' to 1,
     )
 }
