d0387bfe6d9a91844c1ec5d8696b9dff313d4ebf
uknow-mac <yoon6763@naver.com>
2024-11-23 15:44:00 +0900

    Chore: 공통 테마 설정

--- app/src/main/java/com/bikulwon/manseryeok/page/ui/theme/Color.kt
+++ app/src/main/java/com/bikulwon/manseryeok/page/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package com.bikulwon.manseryeok.page.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFBFE)
+val Gray = Color(0xFFBDBDBD)
+val Clear = Color(0x00000000)
+val LightGray = Color(0xFFEEEEEE)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Mint = Color(0xFF4BD0B2)
+val Navy = Color(0xFF293462)
+val Yellow = Color(0xFFFEB139)
+val SkyBlue = Color(0xFFA6D1E6)
+val Crimson = Color(0xFFD61C4E)
+
+val LightBlue = Color(0xFF039BE5)
--- app/src/main/java/com/bikulwon/manseryeok/page/ui/theme/Theme.kt
+++ app/src/main/java/com/bikulwon/manseryeok/page/ui/theme/Theme.kt
@@ -9,27 +9,29 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Navy,
+    secondary = SkyBlue,
+    tertiary = Mint
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = Navy,
+    secondary = SkyBlue,
+    tertiary = Mint,
+    background = White,
+    surface = White,
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
+
+    /* Other default colors to override
+
     */
 )
 
